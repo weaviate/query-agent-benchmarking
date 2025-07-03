6ca7ee4fb57a2e774bcf68eef1aa3f7193db9e87
Connor Shorten <connorshorten300@gmail.com>
2025-07-03 15:50:05 -0400

    Note on QueryAgentCollectionConfig

--- benchmarker/src/agent.py
+++ benchmarker/src/agent.py
@@ -71,6 +71,25 @@ class AgentBuilder:
                 auth_credentials=weaviate.auth.AuthApiKey(self.api_key),
             )
             
+            # NOTE [Named Vectors]: Replace collections: list[str] with list[QueryAgentCollectionConfig]
+            '''
+            class QueryAgentCollectionConfig(BaseModel):
+            """A collection configuration for the QueryAgent.
+
+            Attributes:
+                name: The name of the collection to query.
+                tenant: Tenant name for collections with multi-tenancy enabled.
+                view_properties: Optional list of property names the agent has the ability to view
+                    for this specific collection.
+                target_vector: Optional target vector name(s) for collections with named vectors.
+                    Can be a single vector name or a list of vector names.
+            """
+
+            name: str
+            tenant: Union[str, None] = None
+            view_properties: Union[list[str], None] = None
+            target_vector: Union[str, list[str], None] = None
+            '''
             if agent_name == "query-agent":
                 self.agent = QueryAgent(
                     client=self.weaviate_client,
