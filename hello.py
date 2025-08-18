5102c468aa04245c6dfa1c44eb5b8693459ee93a
Connor Shorten <connorshorten300@gmail.com>
2025-08-18 14:42:18 -0400

    clean

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from query-agent-benchmarking!")
-
-
-if __name__ == "__main__":
-    main()
--- scripts/load_dataset.py
+++
@@ -1,5 +0,0 @@
-from src.dataset import load_dataset
-
-dataset = load_dataset("./weaviate-gorilla.json")
-
-print(len(dataset))
